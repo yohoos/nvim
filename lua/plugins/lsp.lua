250865fcd31deec9effa4385aaade82f9e424d54
Yuhua Ni <yuhua.ni@reddit.com>
2025-08-08 15:41:00 -0400

    update packages

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -77,7 +77,9 @@ return {
             "nvim-treesitter/nvim-treesitter",
         },
         config = function()
-            require("go").setup({ luasnip = true })
+            require("go").setup({
+                luasnip = true,
+            })
         end,
         event = {"CmdlineEnter"},
         ft = {"go", 'gomod'},
